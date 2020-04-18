b8a83da85b35941173cacbb2f0d80adbdc1a773a
vldkvll <vldkvll.gmail.com>
2020-04-19 02:38:12 +0300

    Updates

---
+++ precache-manifest.79e489e83d58189ba151762d854dd532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5571ddc1e8e2b0852be965623eb1c550",
+    "url": "/COVID-19-meter/index.html"
+  },
+  {
+    "revision": "08c4ded75d4852ce45e7",
+    "url": "/COVID-19-meter/static/css/main.1adb46a6.chunk.css"
+  },
+  {
+    "revision": "dcdb5b2ea43a5ffb20e4",
+    "url": "/COVID-19-meter/static/js/2.47e6375e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-meter/static/js/2.47e6375e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c4ded75d4852ce45e7",
+    "url": "/COVID-19-meter/static/js/main.3104e776.chunk.js"
+  },
+  {
+    "revision": "903e6707682d71a895b2",
+    "url": "/COVID-19-meter/static/js/runtime-main.19e7b677.js"
+  },
+  {
+    "revision": "7fcff41e93a8a8323918ee1ac3abe724",
+    "url": "/COVID-19-meter/static/media/covid-logo.7fcff41e.jpg"
+  }
+]);
